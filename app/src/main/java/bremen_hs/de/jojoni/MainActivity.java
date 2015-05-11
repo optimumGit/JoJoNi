42fc8a18533cbcf913b747eefbdad03a65dd4a69
DerChief <nikoritter@gmail.com>
2015-05-11 18:00:32 +0200

    Servus

--- app/src/main/java/bremen_hs/de/jojoni/MainActivity.java
+++ app/src/main/java/bremen_hs/de/jojoni/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-//Hallo chier is hh
+//Hallo chier is da Chief
 
 public class MainActivity extends ActionBarActivity {
 
