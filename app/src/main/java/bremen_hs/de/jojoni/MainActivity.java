12821baa29c2787aa455741c2e539d40711c5748
JohannaSchmidt <johanna._schmidt@web.de>
2015-05-11 18:02:06 +0200

    TestJojo

--- app/src/main/java/bremen_hs/de/jojoni/MainActivity.java
+++ app/src/main/java/bremen_hs/de/jojoni/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    // Hallo Welt!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
