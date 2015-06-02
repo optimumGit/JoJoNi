f029068128fa25f5114a88a5ccdb97fc5b08033c
JohannaSchmidt <johanna._schmidt@web.de>
2015-06-02 11:56:59 +0200

    Google Play Services setup

--- app/src/main/java/bremen_hs/de/jojoni/MainActivity.java
+++ app/src/main/java/bremen_hs/de/jojoni/MainActivity.java
@@ -1,5 +1,6 @@
 package bremen_hs.de.jojoni;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     //HalloWelt!!
     @Override
