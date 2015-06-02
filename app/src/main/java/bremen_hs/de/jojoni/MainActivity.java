1795f62d066c81e9dc5edadea321ab8ab8896232
JohannaSchmidt <johanna._schmidt@web.de>
2015-06-02 14:42:56 +0200

    Google Play Services setup

--- app/src/main/java/bremen_hs/de/jojoni/MainActivity.java
+++ app/src/main/java/bremen_hs/de/jojoni/MainActivity.java
@@ -1,6 +1,7 @@
 package bremen_hs.de.jojoni;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,15 +9,46 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.multiplayer.Invitation;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.OnInvitationReceivedListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.turnbased.OnTurnBasedMatchUpdateReceivedListener;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatch;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatchConfig;
+import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMultiplayer;
+import com.google.android.gms.plus.Plus;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        OnInvitationReceivedListener, OnTurnBasedMatchUpdateReceivedListener {
 
-    //HalloWelt!!
+    private static final String TAG = "SekaCardGame";
+    private static final int RC_SELECT_PLAYERS = 10000;
+
+    private GoogleApiClient apiClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create the Google API Client with access to Plus and Games
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
 
         // Der Regeln anzeigen Button wird mit der Funktion verknüpft
         Button buttonShowRules = (Button) findViewById(R.id.btnShowRules);
@@ -32,7 +64,7 @@ public class MainActivity extends Activity {
         buttonCreateGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Funktion
+                onStartMatchClicked();
             }
         });
 
@@ -46,7 +78,78 @@ public class MainActivity extends Activity {
         });
 
     }
-//Test Commit & Push
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Connect ApiClient ... ");
+        apiClient.connect();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Disconnect ApiClient ... ");
+        if (apiClient.isConnected()) {
+            apiClient.disconnect();
+        }
+    }
+
+
+    private void onStartMatchClicked() {
+        // Select between 1 and 3 players (not including the current one, so the game has 2-4 total)
+        int minPlayers = 1;
+        int maxPlayers = 8;
+        Intent intent = Games.TurnBasedMultiplayer.getSelectOpponentsIntent(apiClient,
+                minPlayers, maxPlayers, true);
+        startActivityForResult(intent, RC_SELECT_PLAYERS);
+    }
+
+    @Override
+    public void onActivityResult(int request, int response, Intent data) {
+        super.onActivityResult(request, response, data);
+
+        if (request == RC_SELECT_PLAYERS) {
+            if (response != Activity.RESULT_OK) {
+                // user canceled
+                Log.d(TAG, "onActivityResult: user canceled player selection.");
+                return;
+            }
+
+            // get the invitee list
+            final ArrayList<String> invitees = data
+                    .getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
+
+            // get automatch criteria
+            Bundle autoMatchCriteria = null;
+
+            int minAutoMatchPlayers = data.getIntExtra(
+                    Multiplayer.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
+            int maxAutoMatchPlayers = data.getIntExtra(
+                    Multiplayer.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
+
+            if (minAutoMatchPlayers > 0) {
+                autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
+                        minAutoMatchPlayers, maxAutoMatchPlayers, 0);
+            } else {
+                autoMatchCriteria = null;
+            }
+
+            TurnBasedMatchConfig tbmc = TurnBasedMatchConfig.builder()
+                    .addInvitedPlayers(invitees)
+                    .setAutoMatchCriteria(autoMatchCriteria).build();
+
+            // Start the match
+            Games.TurnBasedMultiplayer.createMatch(apiClient, tbmc).setResultCallback(
+                    new ResultCallback<TurnBasedMultiplayer.InitiateMatchResult>() {
+                        @Override
+                        public void onResult(TurnBasedMultiplayer.InitiateMatchResult result) {
+                            processResult(result);
+                        }
+                    });
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,5 +171,46 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "Connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection failed");
+    }
+
+    @Override
+    public void onInvitationReceived(Invitation invitation) {
+
+    }
+
+    @Override
+    public void onInvitationRemoved(String s) {
+
+    }
+
+    @Override
+    public void onTurnBasedMatchReceived(TurnBasedMatch turnBasedMatch) {
+
+    }
+
+    @Override
+    public void onTurnBasedMatchRemoved(String s) {
+
+    }
+
+
+    private void processResult(TurnBasedMultiplayer.InitiateMatchResult result) {
+
+        Toast.makeText(this, "Start Game", Toast.LENGTH_LONG).show();
     }
 }
